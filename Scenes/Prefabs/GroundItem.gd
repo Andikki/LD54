0b2c8170bdadded3a37c9c24913ca468b8ffc4da
Tresillo <robin.cowling2122@gmail.com>
2023-10-01 16:46:01 +0100

    Made the Ground items place on the floor in the middle of the tile

--- Scenes/Prefabs/GroundItem.gd
+++ Scenes/Prefabs/GroundItem.gd
@@ -7,8 +7,6 @@ var tile_size = 32
 
 
 func _ready() -> void:
-	print("hi")
 	$Sprite2D.texture = item.sprite
 	#find position if item
-	position = grid_pos * tile_size
-	print(str(position))
+	position = grid_pos * tile_size - Vector2i(tile_size/2, tile_size/2)
