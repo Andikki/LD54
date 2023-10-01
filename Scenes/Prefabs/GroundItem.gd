724eadd41fad8ca00df993d57641a04d192068cf
Tresillo <robin.cowling2122@gmail.com>
2023-10-01 16:54:52 +0100

    Small Changes thinking about ground items with light

--- Scenes/Prefabs/GroundItem.gd
+++ Scenes/Prefabs/GroundItem.gd
@@ -2,11 +2,14 @@ extends Node2D
 
 @export var grid_pos: Vector2i
 @export var item: Item
+#@export var lights: Array[LightSource]
 
-var tile_size = 32
+var tile_size:int = 32
 
 
 func _ready() -> void:
 	$Sprite2D.texture = item.sprite
 	#find position if item
 	position = grid_pos * tile_size - Vector2i(tile_size/2, tile_size/2)
+	
+	
