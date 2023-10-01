4aae33f2268842d8d1e7b7026340533a4e820019
Tresillo <robin.cowling2122@gmail.com>
2023-10-01 23:11:46 +0100

    Created a static body on the inherited item scene, which is disabled by default.

--- Scenes/Prefabs/Item.gd
+++ Scenes/Prefabs/Item.gd
@@ -10,13 +10,24 @@ enum Location {GROUND, HAND}
 	set(value): 
 		location = value
 		adjust_sprite_position()
+@export var has_collision: bool
 #@export var ingredient_in
 
 func _init(m_item_name: String = ""):
 	item_name = m_item_name
-	
+
+func _ready():
+	if location == Location.GROUND and has_collision:
+		$StaticBody2D/CollisionShape2D.disabled = false
+	else:
+		$StaticBody2D/CollisionShape2D.disabled = true
+
 func adjust_sprite_position() -> void:
 	if location == Location.GROUND:
 		$SpriteContainer.position = $TileCentreAnchor.position
 	elif  location == Location.HAND:
 		$SpriteContainer.position = $HandAnchor.position
+
+
+func _on_click_target_area_input_event(viewport, event, shape_idx):
+	print("Item clicked with name: " + item_name)
