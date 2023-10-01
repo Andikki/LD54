5900ecd92cf52bec7102de9dc9f51bbc3e1329a7
Tresillo <robin.cowling2122@gmail.com>
2023-10-01 16:37:41 +0100

    Added placeholder Item with Item resource on the ground. Is collidable, but places offset due to the tilemap being offset

--- Scenes/Prefabs/GroundItem.gd
+++ Scenes/Prefabs/GroundItem.gd
@@ -5,13 +5,10 @@ extends Node2D
 
 var tile_size = 32
 
-#constructor
-func _init(grid_position: Vector2i, item_type: Item):
-	grid_pos = grid_position
-	item = item_type
-
 
 func _ready() -> void:
+	print("hi")
 	$Sprite2D.texture = item.sprite
 	#find position if item
 	position = grid_pos * tile_size
+	print(str(position))
