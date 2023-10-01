ef761f72f9d90356beae66fb6f5696a955d5d7be
Tresillo <robin.cowling2122@gmail.com>
2023-10-01 15:57:00 +0100

    Made Item into a Resource for Ground Item

--- Scenes/Prefabs/GroundItem.gd
+++ Scenes/Prefabs/GroundItem.gd
@@ -1,11 +1,17 @@
 extends Node2D
 
-var grid_pos: Vector2i
-var item: Item
+@export var grid_pos: Vector2i
+@export var item: Resource
+
+var tile_size = 32
 
 #constructor
 func _init(grid_position: Vector2i, item_type: Item):
 	grid_pos = grid_position
 	item = item_type
+
+
+func _ready() -> void:
 	$Sprite2D.texture = item.sprite
-	
+	#find position if item
+	position = grid_pos * tile_size
--- Scripts/Item.gd
+++ Scripts/Item.gd
@@ -1,16 +1,11 @@
-extends Node
+extends Resource
 
 class_name Item
 
-var title: String
-var sprite_path: String
-var sprite: Texture2D
-var ingredient_in
+@export var name: String
+@export var sprite: Texture2D
+#@export var ingredient_in
 
-func _init(title_name: String, sprite_file_path: String):
-	title = title_name
-	sprite_path = sprite_file_path
-	if sprite_path is String and sprite_path.is_valid_filename():
-		sprite = load(sprite_path)
-	else:
-		push_error("Could not find sprite path for item: " + title)
+func _init(m_name: String = "", m_sprite: Texture2D = Texture2D.new()):
+	name = m_name
+	sprite = m_sprite
