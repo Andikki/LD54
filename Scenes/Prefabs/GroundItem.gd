e502cd975436603b7e4731c288972a1b1b26238f
Tresillo <80215793+Tresillo@users.noreply.github.com>
2023-10-01 14:34:06 +0100

    Items on ground (#9)
    * Ground Item Scene
    
    * Created Item Class to be held in ground item and hands
    
    * Added Functionality for changing sprites of the generic item based on the file path plus validation

---
+++ Scenes/Prefabs/GroundItem.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+var grid_pos: Vector2i
+var item: Item
+
+#constructor
+func _init(grid_position: Vector2i, item_type: Item):
+	grid_pos = grid_position
+	item = item_type
+	$Sprite2D.texture = item.sprite
+	
