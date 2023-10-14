0b54882729fc89466f5edc0e84d45ffcd6b8ca14
Andikki <andikki@outlook.com>
2023-10-14 17:11:42 +0300

    Title screen updated with new image

--- Scenes/Levels/TitleScreen.gd
+++ Scenes/Levels/TitleScreen.gd
@@ -1,6 +1,6 @@
-extends Node2D
+extends Control
 
 @export var game_scene: PackedScene
 
-func _on_main_menu_button_pressed() -> void:
+func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_packed(game_scene)
