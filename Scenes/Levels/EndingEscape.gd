2cb77a1730b4a2237e160ef67220768d5aa70e75
Andikki <andikki@outlook.com>
2023-10-15 04:22:19 +0300

    Forgotten rename

--- Scenes/Levels/EndingEscape.gd
+++ Scenes/Levels/EndingEscape.gd
@@ -1,4 +1,4 @@
 extends Control
 
 func _on_to_title_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Levels/SplashScreen.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Levels/TitleScreen.tscn")
